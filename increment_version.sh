89f62da722b115514d58f095bee981522db94581
Paul Laffitte <paul.laffitte@epitech.eu>
2018-11-13 12:10:41 +0100

    support version tags

--- increment_version.sh
+++ increment_version.sh
@@ -1,27 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Increment a version string using Semantic Versioning (SemVer) terminology.
 function semverIncr
 {
-  # Parse command line options.
-  while getopts ":Mmp" Option; do
-    case $Option in
+  local OPTIND Option major minor patch tag version
+  while getopts ":Mmpt:" Option; do
+    case "$Option" in
       M ) major=true;;
       m ) minor=true;;
       p ) patch=true;;
+      t ) tag="-${OPTARG}";;
     esac
   done
-
   shift $(($OPTIND - 1))
 
-  version=$1
+  version=$(expr "$1" : '\(.*\)\-.*' || echo "$1")
+  if [ -z "$tag" ]; then
+    tag=$(expr "$1" : '.*\(\-.*\)')
+  elif [ "$tag" == "-" ]; then
+    tag=''
+  fi
 
   # Build array from version string.
   a=( ${version//./ } )
 
   # If version string is missing or has the wrong number of members, show usage message.
   if [ ${#a[@]} -ne 3 ]; then
-    echo "usage: $(basename $0) [-Mmp]"
+    echo "usage: $(basename $0) [-Mmpt:]"
     exit 1
   fi
 
@@ -41,16 +46,16 @@ function semverIncr
     ((a[2]++))
   fi
 
-  echo "${a[0]}.${a[1]}.${a[2]}"
+  echo "${a[0]}.${a[1]}.${a[2]}$tag"
 }
 
-if ! [[ -f .semver ]]; then
+if ! [ -f .semver ]; then
   VERSION='0.0.1-beta'
   echo "$VERSION" > .semver
 else
   VERSION=$(cat .semver)
 fi
 
-NEW_VERSION=$(semverIncr "$1" $VERSION)
+NEW_VERSION=$(semverIncr "$@" $VERSION)
 echo $NEW_VERSION
 echo $NEW_VERSION > .semver
